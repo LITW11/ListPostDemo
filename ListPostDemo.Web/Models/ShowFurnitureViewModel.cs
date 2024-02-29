60f1f2de007227e4a0c9bd66ab9b489b772b50a8
Avrumi Friedman <a.friedman07@gmail.com>
2024-02-28 22:16:49 -0500

    Add project files.

---
+++ ListPostDemo.Web/Models/ShowFurnitureViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ListPostDemo.Data;
+
+namespace ListPostDemo.Web.Models
+{
+    public class ShowFurnitureViewModel
+    {
+        public List<FurnitureItem> FurnitureItems { get; set; }
+        public string Message { get; set; }
+    }
+}
